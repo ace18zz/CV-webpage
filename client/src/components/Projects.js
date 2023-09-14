d107978bcfccc3ea70be6eeea830a5e369cafdc9
Ziting Zhao <Cocomeri17@gmail.com>
2023-09-14 20:00:11 +0100

    homepage

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,35 +1,48 @@
 import React from 'react';
+import { motion } from "framer-motion";
+
 
 const projectsData = [
   {
     title: "Project 1",
     description: "This is a brief description of Project 1.",
-    imageUrl: "path_to_image_1.jpg",
-    projectUrl: "http://example.com"
+    imageUrl: "rock.png",
+    projectUrl: "https://github.com/ace18zz/Rock-Paper-Scissors-Multiplayer-Game"
   },
   {
     title: "Project 2",
     description: "This is a brief description of Project 1.",
-    imageUrl: "path_to_image_1.jpg",
-    projectUrl: "http://example.com"
+    imageUrl: "news-page.png",
+    projectUrl: "https://github.com/ace18zz/news-summary-challenge"
   },
   // ... other projects
 ];
 
+
 function Projects() {
   return (
     <div className="projects-section">
-      {projectsData.map(project => (
-        <div key={project.title} className="project-card">
-          <img src={project.imageUrl} alt={project.title} />
+      {projectsData.map((project, index) => (
+        <motion.div 
+          key={project.title} 
+          className="project-card"
+          initial={{ opacity: 0, x: 200 }}  
+          animate={{ opacity: 1, x: 0 }}  
+          transition={{ delay: 0.2 * index, duration: 0.8 }}
+          whileHover={{ scale: [null, 1.5, 1.4], zIndex: 10 }}  // 鼠标悬停的动画效果
+        >
+          <a href={project.projectUrl} target="_blank" rel="noopener noreferrer">
+            <img src={project.imageUrl} alt={project.title} />
+          </a>
           <h2>{project.title}</h2>
           <p>{project.description}</p>
-          <a href={project.projectUrl} target="_blank" rel="noopener noreferrer">View Project</a>
-        </div>
+
+        </motion.div>
       ))}
-      
     </div>
   );
 }
+
+
 
 export default Projects;
